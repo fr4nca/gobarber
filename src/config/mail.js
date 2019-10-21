b2d995b4000a84b6de6d877f28aafd85084cad3e
Victor França <victor.franca@confi.gr>
2019-10-21 20:41:58 -0300

    nodemailer

---
+++ src/config/mail.js
@@ -0,0 +1,12 @@
+export default {
+  host: 'smtp.mailtrap.io',
+  port: 2525,
+  secure: false,
+  auth: {
+    user: '43c8747cf5d9ab',
+    pass: '84d2d050d8cbea',
+  },
+  default: {
+    from: 'GoBarber Team <noreply@gobarber.com>',
+  },
+};
